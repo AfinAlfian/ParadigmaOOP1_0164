6c598cd822f25b82c625c29e1858d3b74b0c6afb
AfinAlfian <alfinalfiansyah528@gmail.com>
2024-05-21 16:42:04 +0700

    menghapus

--- Association/Association.cpp
+++ Association/Association.cpp
@@ -74,5 +74,12 @@ int main()
     varDokter1->cetakPasien();
     varPasien1->cetakDokter();
     varPasien2->cetakDokter();
+
+    delete varPasien1;
+    delete varPasien2;
+    delete varDokter1;
+    delete varDokter2;
+
+    return 0;
 }
 
