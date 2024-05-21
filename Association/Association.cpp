279417b89ff9f391921ffb9f9494b7527c7df14c
AfinAlfian <alfinalfiansyah528@gmail.com>
2024-05-21 16:38:55 +0700

    menambahkan class dokter beserta membernya

--- Association/Association.cpp
+++ Association/Association.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 using namespace std;
 
+class dokter;
 class pasien {
 public:
     string nama;
@@ -15,6 +16,22 @@ public:
     }
     void tambahDokter(dokter*);
     void cetakDokter();
+};
+
+class dokter {
+public:
+    string nama;
+    vector<pasien*> daftar_pasien;
+
+    dokter(string pNama) :nama(pNama) {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+    ~dokter() {
+        cout << "Dokter \"" << nama << "\" tidak ada\n";
+    }
+
+    void tambahPasien(pasien*);
+    void cetakPasien();
 };
 
 int main()
