91f3773b2826c6677f70a3774147e58562b034a8
AfinAlfian <alfinalfiansyah528@gmail.com>
2024-05-21 16:40:20 +0700

    mengisis fungsi tambahpasien

--- Association/Association.cpp
+++ Association/Association.cpp
@@ -46,6 +46,11 @@ void pasien::cetakDokter() {
     cout << endl;
 }
 
+void dokter::tambahPasien(pasien* pPasien) {
+    daftar_pasien.push_back(pPasien);
+    pPasien->tambahDokter(this);
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
